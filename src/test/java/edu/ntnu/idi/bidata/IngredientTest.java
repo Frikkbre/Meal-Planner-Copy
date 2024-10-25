2fe92ba878fc69f4b87a410a0022bad893aaacbb
Frikk Brændsrød <frikkb@ntnu.no>
2024-10-25 12:02:55 +0200

    Generated test class and generated framework

---
+++ src/test/java/edu/ntnu/idi/bidata/IngredientTest.java
@@ -0,0 +1,52 @@
+package edu.ntnu.idi.bidata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientTest {
+
+    @org.junit.jupiter.api.Test
+    void setIngredientName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIngredientExpirationDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIngredientIsExpired() {
+    }
+
+    @Test
+    void testSetIngredientName() {
+    }
+
+    @Test
+    void setIngredientType() {
+    }
+
+    @Test
+    void setIngredientPrice() {
+    }
+
+    @Test
+    void setIngredientAmount() {
+    }
+
+    @Test
+    void setIngredientUnit() {
+    }
+
+    @Test
+    void testSetIngredientExpirationDate() {
+    }
+
+    @Test
+    void testSetIngredientIsExpired() {
+    }
+
+    @Test
+    void displayInformation() {
+    }
+}
