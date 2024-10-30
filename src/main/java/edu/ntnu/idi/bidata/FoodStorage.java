61b956d3bdf35d0cbccd35ec3a5a2b0282d630f2
Frikk Brændsrød <fribra05@yahoo.com>
2024-10-30 14:23:14 +0100

    Cleaned up FoodStorage file and deleted duplicate code (duplicate from UserInteraction)

--- src/main/java/edu/ntnu/idi/bidata/FoodStorage.java
+++ src/main/java/edu/ntnu/idi/bidata/FoodStorage.java
@@ -1,13 +1,18 @@
 package main.java.edu.ntnu.idi.bidata;
 
+import main.edu.ntnu.idi.bidata.Ingredient;
+
+import java.util.Scanner;
+
 public class FoodStorage {
     /**
      * Should have fields like HashMap(key = ingredient, value = amount) used for recipes.
      * other fields like
      *
-     * take userinput here? add food and such.
+     * take userinput here?
      */
-    public static void main(String[]args){
-        System.out.println("Hello World!");
+
+    //TODO - change file to hold information about objects created with UI and store them in a list or hashmap.
+
     }
 }
