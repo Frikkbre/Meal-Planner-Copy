df32b004356fdc32cf85878a58093aa5d0ba42b4
Frikk Brændsrød <fribra05@yahoo.com>
2024-11-16 16:48:08 +0100

    Made constructor and made app object call startApplication

--- src/main/java/edu/ntnu/idi/bidata/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/Main.java
@@ -1,8 +1,26 @@
 package edu.ntnu.idi.bidata;
 
+import edu.ntnu.idi.bidata.userInteraction.MealPlannerApp;
+
 /**
  * The main starting point of your application. Let this class create the
  * instance of your main-class that starts your application.
  */
 public class Main {
+
+  public Main() {
+    MealPlannerApp mealPlannerApp = new MealPlannerApp();
+    mealPlannerApp.startApplication();
+  }
+
+
+  /**
+   * The main method of the application.
+   *
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Main application = new Main();
+
+  }
 }
