17128172004fac2a86bec79be51a73aa08b9fe9e
Frikk Brændsrød <fribra05@yahoo.com>
2024-11-19 21:27:55 +0100

    Changed menu to two stage, needs revision on search ingredient skipping.

--- src/main/java/edu/ntnu/idi/bidata/userInterface/MealPlannerApp.java
+++ src/main/java/edu/ntnu/idi/bidata/userInterface/MealPlannerApp.java
@@ -2,6 +2,7 @@ package edu.ntnu.idi.bidata.userInterface;
 
 import edu.ntnu.idi.bidata.FoodStorage;
 import edu.ntnu.idi.bidata.entity.Ingredient;
+
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -12,53 +13,77 @@ public class MealPlannerApp {
   /**
    * Should have fields like HashMap(key = ingredient, value = amount) used for recipes.
    * other fields like
-   *<p></p>
+   * <p></p>
    * take userInput here? add food and such. yes
    */
 
-  public static void startApplication(){
+  public static void startApplication() {
     Scanner inputScanner = new Scanner(System.in);
     FoodStorage foodStorage = new FoodStorage();
     boolean running = true;
 
-    while(running) {
+    while (running) {
 
       System.out.println("What do you want to do?");
-      System.out.println("1 = Add ingredient");
-      System.out.println("2 = remove ingredient");
-      System.out.println("3 = search ingredient");
-      System.out.println("4 = find a recipe");
-      System.out.println("5 = exit the program");
+      System.out.println("1 = Modify ingredient");
+      System.out.println("2 = Modify recipe");
+      System.out.println("3 = exit the program");
 
       int inputChoice = inputScanner.nextInt();
       inputScanner.nextLine();
 
       switch (inputChoice) {
         case 1:
-          foodStorage.addIngredient();
-
-          break;
-        case 2:
-          System.out.println("What is the name of the ingredient you want to remove?");
-          String ingredientName = inputScanner.nextLine();
-
-          System.out.println("How much/many do you want to remove?");
-          int amountToRemove = inputScanner.nextInt();
+          System.out.println("1 = Add ingredient");
+          System.out.println("2 = Remove ingredient");
+          System.out.println("3 = Search ingredient");
+          int ingredientChoice = inputScanner.nextInt();
           inputScanner.nextLine();
+          switch (ingredientChoice) {
+            case 1:
+              foodStorage.addIngredient();
 
-          foodStorage.removeIngredient(ingredientName, amountToRemove);
+              break;
+            case 2:
+              System.out.println("What is the name of the ingredient you want to remove?");
+              String ingredientName = inputScanner.nextLine();
 
+              System.out.println("How much/many do you want to remove?");
+              int amountToRemove = inputScanner.nextInt();
+              inputScanner.nextLine();
+
+              foodStorage.removeIngredient(ingredientName, amountToRemove);
+
+              break;
+            case 3:
+              System.out.println("What is the name of the ingredient you want to search for?");
+              ingredientName = inputScanner.nextLine();
+
+              foodStorage.searchIngredient(ingredientName);
+              break;
+          }
+          break;
+
+        case 2:
+
+          System.out.println("1 = Add recipe");
+          System.out.println("2 = remove recipe");
+          System.out.println("3 = search recipe");
+          System.out.println("What do you want to do with recipies?");
+          int recipeChoice = inputScanner.nextInt();
+          switch (recipeChoice) {
+            case 1:
+              //addRecipe();
+              break;
+            case 2:
+              //removeRecipe();
+              break;
+            case 3:
+              //searchRecipe();
+              break;
+          }
           break;
         case 3:
-          System.out.println("What is the name of the ingredient you want to search for?");
-          ingredientName = inputScanner.nextLine();
-
-          foodStorage.searchIngredient(ingredientName);
-          break;
-        case 4:
-          //findRecipe();
-          break;
-        case 5:
           running = false;
           System.out.println("Exiting the program");
           System.out.println("Have a nice day!");
