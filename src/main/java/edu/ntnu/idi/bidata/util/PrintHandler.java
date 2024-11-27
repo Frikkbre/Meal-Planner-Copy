f6f3056c21455cd58512a332e6fd8d759e396480
Frikk Brændsrød <fribra05@yahoo.com>
2024-11-28 00:43:21 +0100

    Added main menu, ingredient menu and recipe menu to printHandler

--- src/main/java/edu/ntnu/idi/bidata/util/PrintHandler.java
+++ src/main/java/edu/ntnu/idi/bidata/util/PrintHandler.java
@@ -1,6 +1,10 @@
 package edu.ntnu.idi.bidata.util;
 
 import edu.ntnu.idi.bidata.entity.Ingredient;
+import edu.ntnu.idi.bidata.entity.Recipe;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class PrintHandler {
   public static void printIngredient(Ingredient ingredient) {
@@ -13,5 +17,51 @@ public class PrintHandler {
     System.out.println("Is Expired: " + ingredient.getIngredientIsExpired());
     System.out.println();
 
+    /*try { //TODO - Write AI declaration
+      TimeUnit.SECONDS.sleep(5);
+    } catch (InterruptedException e) {
+      System.out.println("Sleep failed: " + e.getMessage());
+      Thread.currentThread().interrupt(); // Reset the interrupt flag
+    }*/
+
+  }
+
+  public static void printRecipe(Recipe recipe) {
+
+     System.out.println("Name: " + recipe.getRecipeName());
+     System.out.println("Description: " + recipe.getRecipeDescription());
+     System.out.println("Ingredients: ");
+    for (String ingredient : recipe.getRecipeIngredients().keySet()) {
+      System.out.println(ingredient + ": " + recipe.getRecipeIngredients().get(ingredient));
+    }
+    System.out.println("Instructions: " + recipe.getRecipeInstructions());
+    System.out.println();
+  }
+
+  /**
+   * Prints menu 1 options for user, this includes:
+   * 1. modify recipe
+   * 2. modify recipe
+   * 3. exit program
+   */
+  public static void printMainMenuOption() {
+    System.out.println("What do you want to do?");
+    System.out.println("1 = Modify ingredient");
+    System.out.println("2 = Modify recipe");
+    System.out.println("0 = exit the program");
+  }
+
+  public static void printMenuIngredientOption() {
+    System.out.println("1 = Add ingredient");
+    System.out.println("2 = Remove ingredient");
+    System.out.println("3 = Search ingredient");
+    System.out.println("4 = Show ingredients");
+    System.out.println("5 = Show expired ingredients");
+  }
+
+  public static void printMenuRecipeOption() {
+    System.out.println("1 = Add recipe");
+    System.out.println("2 = remove recipe");
+    System.out.println("3 = search recipe");
   }
 }
