8531910a76ab837fecb06b9d3bd2008cba654379
Frikk Brændsrød <fribra05@yahoo.com>
2024-11-29 17:32:39 +0100

    Updated documentation for Recipe class

--- src/main/java/edu/ntnu/idi/bidata/entity/Recipe.java
+++ src/main/java/edu/ntnu/idi/bidata/entity/Recipe.java
@@ -1,16 +1,38 @@
 package edu.ntnu.idi.bidata.entity;
 
-public class Recipe { //TODO - Should this be hardcoded or should it be user input?
+import java.util.HashMap;
+
+/**
+ * The {@code Recipe} class is responsible for creating recipe.
+ * It provides methods to set and get recipe name, description, ingredients and instructions.
+ * <p>
+ *  This class can pass recipes to CookBook where the recipes are stored.
+ *
+ * @version 0.0.2
+ * @since 29.11.2024
+ * @author Frikk Brændsrød
+ */
+public class Recipe {
   private String recipeName;
   private String recipeDescription;
-  private String recipeIngredients;
+  private HashMap <String, Integer> recipeIngredients = new HashMap<>();
   private String recipeInstructions;
 
-
+  /**
+   * returns the name of the recipe
+   *
+   * @return the name of the recipe
+   */
   public String getRecipeName() {
     return recipeName;
   }
 
+  /**
+   * sets the name of the recipe and checks for illegal arguments
+   * Illegal arguments are null or empty strings
+   *
+   * @param recipeName
+   */
   public void setRecipeName(String recipeName) {
     if (recipeName == null || recipeName.isEmpty()) {
       throw new IllegalArgumentException("Recipe name cannot be null or empty");
@@ -19,10 +41,20 @@ public class Recipe { //TODO - Should this be hardcoded or should it be user inp
     }
   }
 
+  /**
+   * returns the description of the recipe
+   * @return the description of the recipe
+   */
   public String getRecipeDescription() {
     return recipeDescription;
   }
 
+  /**
+   * sets the description of the recipe and checks for illegal arguments
+   * Illegal arguments are null or empty strings
+   *
+   * @param recipeDescription
+   */
   public void setRecipeDescription(String recipeDescription) {
     if(recipeDescription == null || recipeDescription.isEmpty()){
       throw new IllegalArgumentException("Recipe description cannot be null or empty");
@@ -31,22 +63,53 @@ public class Recipe { //TODO - Should this be hardcoded or should it be user inp
     }
   }
 
-  public String getRecipeIngredients() {
+  /**
+   * returns the ingredients of the recipe
+   * @return the ingredients of the recipe
+   */
+  public HashMap <String, Integer> getRecipeIngredients() {
     return recipeIngredients;
   }
 
-  public void setRecipeIngredients(String recipeIngredients) {
-    if(recipeIngredients == null || recipeIngredients.isEmpty()){
+  /**
+   * sets the ingredients of the recipe and checks for illegal arguments
+   * Illegal arguments are null or empty strings
+   *<p>
+   *   is ran through a loop to check if the amount of the ingredient is negative
+   *   if it is, it throws an IllegalArgumentException
+   *</p>
+   *
+   * @param recipeIngredients
+   */
+  public void setRecipeIngredients(HashMap<String, Integer> recipeIngredients) {
+    if (recipeIngredients == null || recipeIngredients.isEmpty() ||
+        recipeIngredients.containsKey(null) || recipeIngredients.containsValue(null)) {
       throw new IllegalArgumentException("Recipe ingredients cannot be null or empty");
-    } else {
-      this.recipeIngredients = recipeIngredients;
     }
+
+    for (Integer amount : recipeIngredients.values()) {
+      if (amount < 0) {
+        throw new IllegalArgumentException("Ingredient amount cannot be negative");
+      }
+    }
+
+    this.recipeIngredients = recipeIngredients;
   }
 
+  /**
+   * returns the instructions of the recipe
+   * @return the instructions of the recipe
+   */
   public String getRecipeInstructions() {
     return recipeInstructions;
   }
 
+  /**
+   * sets the instructions of the recipe and checks for illegal arguments
+   * Illegal arguments are null or empty strings
+   *
+   * @param recipeInstructions
+   */
   public void setRecipeInstructions(String recipeInstructions) {
     if(recipeInstructions == null || recipeInstructions.isEmpty()){
       throw new IllegalArgumentException("Recipe instructions cannot be null or empty");
@@ -55,8 +118,16 @@ public class Recipe { //TODO - Should this be hardcoded or should it be user inp
     }
   }
 
-  public Recipe(String recipeName, String recipeDescription, String recipeIngredients, String recipeInstructions,
-                String recipeCategory, String recipeDifficulty, String recipeTime) {
+  /**
+   * Constructor for the Recipe class
+   * Used to create a new recipe
+   *
+   * @param recipeName
+   * @param recipeDescription
+   * @param recipeIngredients
+   * @param recipeInstructions
+   */
+  public Recipe(String recipeName, String recipeDescription, HashMap recipeIngredients, String recipeInstructions) {
     this.recipeName = recipeName;
     this.recipeDescription = recipeDescription;
     this.recipeIngredients = recipeIngredients;
