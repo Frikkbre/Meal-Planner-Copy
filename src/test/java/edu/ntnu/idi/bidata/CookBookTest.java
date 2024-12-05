dd6a69a31df26c1626f7c6420f003ee9a80a8334
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-05 20:02:02 +0100

    Created file for testing the CookBook class

--- src/test/java/edu/ntnu/idi/bidata/CookBookTest.java
+++ src/test/java/edu/ntnu/idi/bidata/CookBookTest.java
@@ -1,4 +1,11 @@
 package edu.ntnu.idi.bidata;
+import edu.ntnu.idi.bidata.registry.CookBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CookBookTest {
 }
