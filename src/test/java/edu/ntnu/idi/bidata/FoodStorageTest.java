db43036047ec96df5bd8f82a96f6eb9f5cf00f8f
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-11 15:21:53 +0100

    removed redundant imports

--- src/test/java/edu/ntnu/idi/bidata/FoodStorageTest.java
+++ src/test/java/edu/ntnu/idi/bidata/FoodStorageTest.java
@@ -4,10 +4,6 @@ import edu.ntnu.idi.bidata.registry.FoodStorage;
 
 import edu.ntnu.idi.bidata.userInterface.MealPlannerApp;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class FoodStorageTest {
 
