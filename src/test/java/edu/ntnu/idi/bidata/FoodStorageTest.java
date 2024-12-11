f7af9ecafa9deaa24bb351579876a4e5ac2fd22f
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-11 21:28:00 +0100

    Wrote tests for FoodStorageTest since bug was fixed where it got stuck in main menu

--- src/test/java/edu/ntnu/idi/bidata/FoodStorageTest.java
+++ src/test/java/edu/ntnu/idi/bidata/FoodStorageTest.java
@@ -14,28 +14,49 @@ public class FoodStorageTest {
 
   @BeforeEach
   void setUp(){
-    foodStorage = new FoodStorage(new MealPlannerApp());
+    foodStorage = new FoodStorage();
   }
 
 
   // Returns true when ingredient exists and has sufficient amount
-  /*@Test
-  public void test_has_sufficient_ingredient_amount() {
-    FoodStorage foodStorage = new FoodStorage(new MealPlannerApp());
+  @Test
+  public void hasSufficientIngredientAmountTest() {
+    FoodStorage foodStorage = new FoodStorage();
     Ingredient ingredient = new Ingredient("milk", "dairy", 20.0f, 2.0f, 3, 2024, 12, 31);
     foodStorage.addIngredient(ingredient);
 
     boolean result = foodStorage.hasIngredient("milk", 1.5f);
 
     assertTrue(result);
-  }*/
+  }
 
   @Test
-  public void test_has_sufficient_ingredient_amount() {
+  void hasIngredientTest() {
     Ingredient ingredient = new Ingredient("milk", "dairy", 20.0f, 2.0f, 3, 2024, 12, 31);
     foodStorage.addIngredient(ingredient);
 
     boolean result = foodStorage.hasIngredient("milk", 1.5f);
+
+    assertTrue(result);
+  }
+
+  @Test
+  void addIngredientTest() {
+    Ingredient ingredient = new Ingredient("milk", "dairy", 20.0f, 2.0f, 3, 2024, 12, 31);
+    foodStorage.addIngredient(ingredient);
+
+    boolean result = foodStorage.hasIngredient("milk", 1.5f);
+
+    assertTrue(result);
+  }
+
+  @Test
+  void removeIngredientTest() {
+    Ingredient ingredient = new Ingredient("milk", "dairy", 20.0f, 2.0f, 3, 2024, 12, 31);
+    foodStorage.addIngredient(ingredient);
+    foodStorage.removeIngredient("milk", 1.5f);
+
+    boolean result = foodStorage.hasIngredient("milk", 0.5f); // Check for remaining amount
 
     assertTrue(result);
   }
